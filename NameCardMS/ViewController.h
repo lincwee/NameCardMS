e83145e19713ee11dc1702fdbe147ea5f15e5c3f
lincwee <270335820@qq.com>
2015-04-15 15:37:23 +0800

    Initial Commit

---
+++ NameCardMS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NameCardMS
+//
+//  Created by 郝 林巍 on 15/4/15.
+//  Copyright (c) 2015年 lincwee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NameCardMS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NameCardMS
+//
+//  Created by 郝 林巍 on 15/4/15.
+//  Copyright (c) 2015年 lincwee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
